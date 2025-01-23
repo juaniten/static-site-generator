d949586a229eff8406609802633398c3cd6ea73d
juaniten <juanguijarroferreiro@gmail.com>
2025-01-23 18:48:50 +0100

    added extract functions tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -89,22 +89,38 @@ class TestInlineMarkdown(unittest.TestCase):
         )
 
     def test_extract_markdown_images(self):
-        matches = extract_markdown_images(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
-        )
-        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extract_markdown_images(text), expected)
+
+        text = "No images here!"
+        expected = []
+        self.assertEqual(extract_markdown_images(text), expected)
+
+        text = "Here is one ![test image](https://example.com/image.png)."
+        expected = [("test image", "https://example.com/image.png")]
+        self.assertEqual(extract_markdown_images(text), expected)
+
+        text = "![alt1](url1) ![alt2](url2)"
+        expected = [("alt1", "url1"), ("alt2", "url2")]
+        self.assertEqual(extract_markdown_images(text), expected)
 
     def test_extract_markdown_links(self):
-        matches = extract_markdown_links(
-            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
-        )
-        self.assertListEqual(
-            [
-                ("link", "https://boot.dev"),
-                ("another link", "https://blog.boot.dev"),
-            ],
-            matches,
-        )
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extract_markdown_links(text), expected)
+
+        text = "No links here!"
+        expected = []
+        self.assertEqual(extract_markdown_links(text), expected)
+
+        text = "Here is one [test link](https://example.com)."
+        expected = [("test link", "https://example.com")]
+        self.assertEqual(extract_markdown_links(text), expected)
+
+        text = "[link1](url1) [link2](url2)"
+        expected = [("link1", "url1"), ("link2", "url2")]
+        self.assertEqual(extract_markdown_links(text), expected)
 
 
 if __name__ == "__main__":
